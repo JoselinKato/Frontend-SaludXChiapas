4ea969df02ac25581967f58f865dcf22b4013e5f
Daniel Reyes <danielreher@outlook.com>
2025-11-01 05:32:55 -0600

    Estructura principal del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div>
+      <div className="container mt-4">
+        <Routes>
+          {/* <Route path="/" element={<Login />} /> */}
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
